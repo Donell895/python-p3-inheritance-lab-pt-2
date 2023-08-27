aefd193fd7f5fd6428d08b98b755e2ad1e8bc240
Donell <donell.wambua@student.moringaschool.com>
2023-08-27 08:52:46 +0300

    Completed assignment

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,13 @@
 class Student:
-    pass
+    def hello(self):
+        return "Hey there!"
+    
+    def raise_hand(self):
+        return "Teacher, I have a question."
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        return "Hey there! I'm a chatty student."
+    
+    def raise_hand(self):
+        return "Teacher, I have something to say!"
